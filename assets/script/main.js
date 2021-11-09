8e438b85c7bbf61172cff8331a9799951bdf6598
anistackme <87749180+anistackme@users.noreply.github.com>
2021-11-10 05:00:02 +0700

    Update main.js

--- assets/script/main.js
+++ assets/script/main.js
@@ -1,124 +1,125 @@
-  
-let menuBtn = document.querySelector('#menu-btn');
-let searchBtn = document.querySelector('#search-btn');
-let colorBtn = document.querySelector('#color-btn');
-let themeBtn = document.querySelector('#theme-btn');
-
-let navbar = document.querySelector('.navbar');
-let searchForm = document.querySelector('.search-form');
-let colorsPalette = document.querySelector('.colors-palette');
-
-// aktivasi menu nya
-menuBtn.onclick = () => {
-    navbar.classList.toggle('active');
-    searchForm.classList.remove('active');
-    colorsPalette.classList.remove('active');
-}
-
-// aktivasi search nya
-searchBtn.onclick = () => {
-    searchForm.classList.toggle('active');
-    navbar.classList.remove('active');
-    colorsPalette.classList.remove('active');
-}
-// aktivasi warna nya
-colorBtn.onclick = () => {
-    colorsPalette.classList.toggle('active');
-    navbar.classList.remove('active');
-    searchForm.classList.remove('active');
-}
-
-// ngilangin para bar saat di scroll
-window.onscroll = () => {
-    navbar.classList.remove('active');
-    searchForm.classList.remove('active');
-    colorsPalette.classList.remove('active');
-}
-
-// bagian gonta ganti warna kalo dipencet
-document.querySelectorAll('.colors-palette .color').forEach(btn => {
-    btn.onclick = () => {
-        let color = btn.style.background;
-        document.querySelector(':root').style.setProperty('--main-color', color);
-        console.log();
-    }
-});
-
-
-// buat ngatur/gantiganti darkmode
-themeBtn.onclick = () => {
-    themeBtn.classList.toggle('fa-sun');
-    if (themeBtn.classList.contains('fa-sun')) {
-        document.body.classList.add('active');
-    } else {
-        document.body.classList.remove('active');
-    }
-}
-
-
-var start = new Date();
-var end = new Date();
-var time = new Date().getTime();
+/*==================== SHOW MENU ====================*/
+const showMenu = (toggleId, navId) =>{
+    const toggle = document.getElementById(toggleId),
+    nav = document.getElementById(navId)
     
-//Set the start hours and ending hours
-if (time > start.setHours(18,00) && time < end.setHours(23,59)) {
-    document.body.classList.add('active');
-}
-else {
-    document.body.classList.remove('active');
-}
-
-// include html nih
-function includeHTML() {
-    var z, i, elmnt, file, xhttp;
-    /* Loop through a collection of all HTML elements: */
-    z = document.getElementsByTagName("*");
-    for (i = 0; i < z.length; i++) {
-        elmnt = z[i];
-        /*search for elements with a certain atrribute:*/
-        file = elmnt.getAttribute("w3-include-html");
-        if (file) {
-            /* Make an HTTP request using the attribute value as the file name: */
-            xhttp = new XMLHttpRequest();
-            xhttp.onreadystatechange = function () {
-                if (this.readyState == 4) {
-                    if (this.status == 200) { elmnt.innerHTML = this.responseText; }
-                    if (this.status == 404) { elmnt.innerHTML = "Page not found."; }
-                    /* Remove the attribute, and call this function once more: */
-                    elmnt.removeAttribute("w3-include-html");
-                    includeHTML();
-                }
-            }
-            xhttp.open("GET", file, true);
-            xhttp.send();
-            /* Exit the function: */
-            return;
-        }
+    // verif kalo ada si variabel
+    if(toggle && nav){
+        toggle.addEventListener('click', ()=>{
+            // nambahin show-menu class ke div tag dengan nav_menu class
+            nav.classList.toggle('show-menu')
+        })
     }
 }
+showMenu('nav-toggle','nav-menu')
+
+/*==================== REMOVE MENU MOBILE ====================*/
+const navLink = document.querySelectorAll('.nav_link')
+
+function linkAction(){
+    const navMenu = document.getElementById('nav-menu')
+    // ketika di klik, ngehilangin si menu
+    navMenu.classList.remove('show-menu')
+}
+navLink.forEach(n => n.addEventListener('click', linkAction))
 
 
-//   ini buat fungsi ngilangin si loading
-var loader = document.querySelector(".loader")
-window.addEventListener("load", vanish);
 
-function vanish() {
-    loader.classList.add("disppear");
+
+
+/*==================== SHOW SCROLL TOP ====================*/ 
+function scrollTop(){
+    const scrollTop = document.getElementById('scroll-top');
+    // When the scroll is higher than 560 viewport height, add the show-scroll class to the a tag with the scroll-top class
+    if(this.scrollY >= 560) scrollTop.classList.add('show-scroll'); else scrollTop.classList.remove('show-scroll')
+}
+window.addEventListener('scroll', scrollTop)
+
+
+
+
+
+/*==================== DARK LIGHT THEME ====================*/ 
+const themeButton = document.getElementById('theme-button')
+const darkTheme = 'dark-theme'
+const iconTheme = 'bx-toggle-right'
+
+// Previously selected topic (if user selected)
+const selectedTheme = localStorage.getItem('selected-theme')
+const selectedIcon = localStorage.getItem('selected-icon')
+
+// We obtain the current theme that the interface has by validating the dark-theme class
+const getCurrentTheme = () => document.body.classList.contains(darkTheme) ? 'dark' : 'light'
+const getCurrentIcon = () => themeButton.classList.contains(iconTheme) ? 'bx-toggle-left' : 'bx-toggle-right'
+
+// We validate if the user previously chose a topic
+if (selectedTheme) {
+  // If the validation is fulfilled, we ask what the issue was to know if we activated or deactivated the dark
+  document.body.classList[selectedTheme === 'dark' ? 'add' : 'remove'](darkTheme)
+  themeButton.classList[selectedIcon === 'bx-toggle-left' ? 'add' : 'remove'](iconTheme)
 }
 
+// Activate / deactivate the theme manually with the button
+themeButton.addEventListener('click', () => {
+    // Add or remove the dark / icon theme
+    document.body.classList.toggle(darkTheme)
+    themeButton.classList.toggle(iconTheme)
+    // We save the theme and the current icon that the user chose
+    localStorage.setItem('selected-theme', getCurrentTheme())
+    localStorage.setItem('selected-icon', getCurrentIcon())
+})
 
-// ini buat kolapsible div
-var coll = document.getElementsByClassName("collapsible1");
-var i;
 
-for (i = 0; i < coll.length; i++) {
-  coll[i].addEventListener("click", function() {
-    this.classList.toggle("active");
-    var content = this.nextElementSibling;
-    if (content.style.maxHeight){
-      content.style.maxHeight = null;
-    } else {
-      content.style.maxHeight = content.scrollHeight + "px";
-    } 
+
+
+
+// /*==================== SCROLL REVEAL ANIMATION ====================*/
+// const sr = ScrollReveal({
+//     distance: '30px',
+//     duration: 1800,
+//     reset: true,
+// });
+
+// sr.reveal(`.home_data, .home_img, 
+//            .pricing_data,
+//            .accessory_content,
+//            .footer_content`, {
+//     origin: 'top',
+//     interval: 200,
+// })
+
+// sr.reveal(`.about_img, .event_content`, {
+//     origin: 'left'
+// })
+
+// sr.reveal(`.about_data, .event_img`, {
+//     origin: 'right'
+// })
+
+
+var items = [
+    "ANIMEPICT9",
+    "ANISTACKME"
+  ],
+  $text = $(".js-replace"),
+  delay = 15; //seconds
+
+function loop(delay) {
+  $.each(items, function (i, elm) {
+    $text.delay(delay * 1e3).fadeOut();
+    $text.queue(function () {
+      $text.html(items[i]);
+      $text.dequeue();
+    });
+    $text.fadeIn();
+    $text.queue(function () {
+      if (i == items.length - 1) {
+        loop(delay);
+      }
+      $text.dequeue();
+    });
   });
 }
+
+loop(delay);
+
