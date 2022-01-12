e946f1b14e820fc954ef30029526d14e0282c840
anistackme <87749180+anistackme@users.noreply.github.com>
2022-01-13 02:32:06 +0700

    Update main.js

--- assets/script/main.js
+++ assets/script/main.js
@@ -1,42 +1,5 @@
 document.querySelector('.menu-btn').addEventListener('click', () => document.querySelector('.main-menu').classList.toggle('show'));
 
-// BUAT GANTI-GANTI TITLE
-var items = [
-    "Animepict9",
-    "Anistackme"
-  ],
-  $text = $(".js-replace"),
-  delay = 5; //seconds
-  
-  function loop(delay) {
-  $.each(items, function (i, elm) {
-    $text.delay(delay * 1e3).fadeOut();
-    $text.queue(function () {
-      $text.html(items[i]);
-      $text.dequeue();
-    });
-    $text.fadeIn();
-    $text.queue(function () {
-      if (i == items.length - 1) {
-        loop(delay);
-      }
-      $text.dequeue();
-    });
-  });
-  }
-  
-  loop(delay);
-
-/*==================== SHOW SCROLL TOP ====================*/ 
-function scrollTop(){
-    const scrollTop = document.getElementById('scroll-top');
-    // kalo lewat dari 60 viewport height, nambahin show-scroll class ke tag scroll-top class
-    if(this.scrollY >= 60) scrollTop.classList.add('show-scroll'); else scrollTop.classList.remove('show-scroll')
-  }
-window.addEventListener('scroll', scrollTop)
-
-document.querySelector('.menu-btn').addEventListener('click', () => document.querySelector('.main-menu').classList.toggle('show'));
-
 
 // BUAT GANTI-GANTI TITLE
 var items = [
