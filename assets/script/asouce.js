9e4a23a8fd1205aa55fdeadc12415e78c08b7e98
animepict9.official <106361341+animepict9access@users.noreply.github.com>
2022-05-31 22:28:32 +0700

    Update asouce.js

--- assets/script/asouce.js
+++ assets/script/asouce.js
@@ -1,5 +1,6 @@
 $(document).ready(function(){
 
     $('#mei2022').load("../../../member/asouce/datalink/link-mei-2022.html");
+    $('#juni2022').load("../../../member/asouce/datalink/link-juni-2022.html");
     
 });
